f11b2a44fbb7e569dff653bc9c88b93518855111
Rset Brasil Tecnologia <rsetbrasil@gmail.com>
2025-10-14 22:28:18 +0000

    agora coloque icone Painel Administrativo, em cima

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 import Logo from './Logo';
 import { useCart } from '@/context/CartContext';
 import { Button } from './ui/button';
-import { ShoppingBag } from 'lucide-react';
+import { ShoppingBag, User } from 'lucide-react';
 import { CartSheet } from './CartSheet';
 
 export default function Header() {
@@ -16,17 +16,25 @@ export default function Header() {
         <Link href="/">
           <Logo />
         </Link>
-        <CartSheet>
-            <Button variant="ghost" size="icon" onClick={() => setIsCartOpen(true)} className="relative h-12 w-12">
-                <ShoppingBag className="h-7 w-7" />
-                {cartCount > 0 && (
-                    <span className="absolute top-1 right-1 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-primary-foreground transform translate-x-1/2 -translate-y-1/2 bg-accent rounded-full">
-                    {cartCount}
-                    </span>
-                )}
-                <span className="sr-only">Carrinho de Compras</span>
-            </Button>
-        </CartSheet>
+        <div className="flex items-center gap-2">
+            <Link href="/admin/orders">
+                <Button variant="ghost" size="icon" className="h-12 w-12">
+                    <User className="h-7 w-7" />
+                    <span className="sr-only">Painel Administrativo</span>
+                </Button>
+            </Link>
+            <CartSheet>
+                <Button variant="ghost" size="icon" onClick={() => setIsCartOpen(true)} className="relative h-12 w-12">
+                    <ShoppingBag className="h-7 w-7" />
+                    {cartCount > 0 && (
+                        <span className="absolute top-1 right-1 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-primary-foreground transform translate-x-1/2 -translate-y-1/2 bg-accent rounded-full">
+                        {cartCount}
+                        </span>
+                    )}
+                    <span className="sr-only">Carrinho de Compras</span>
+                </Button>
+            </CartSheet>
+        </div>
       </div>
     </header>
   );
