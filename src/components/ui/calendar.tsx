52e12cc9fda7855c607f2221bf32561127cc6f7e
Rset Brasil Tecnologia <rsetbrasil@gmail.com>
2026-01-10 07:19:04 +0000

    04:16:25.380 Running build in Washington, D.C., USA (East) – iad1 04:16:

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -25,9 +25,9 @@ function Calendar({
       classNames={{
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
-        caption: "flex justify-center pt-1 relative items-center",
+        caption_layout: 'dropdown-buttons',
         caption_label: "text-sm font-medium",
-        caption_dropdowns: "flex justify-center gap-1",
+        caption_dropdowns: "flex justify-center pt-1 relative items-center gap-1",
         nav: "space-x-1 flex items-center",
         nav_button: cn(
           buttonVariants({ variant: "outline" }),
